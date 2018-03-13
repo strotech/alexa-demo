ffc8b5ad601fdc2bbd91129d097db2ab13b07269
strotech <37307710+strotech@users.noreply.github.com>
2018-03-13 03:49:43 +0000

    Create SpeechletRequestStreamHandler.java

---
+++ com.alexa.dem/SpeechletRequestStreamHandler.java
@@ -0,0 +1,27 @@
+package com.alexa.dem;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.amazon.speech.speechlet.lambda.SpeechletRequestStreamHandler;
+
+/**
+ * This class could be the handler for an AWS Lambda function powering an Alexa Skills Kit
+ * experience. 
+ */
+public final class DemoSpeechletRequestStreamHandler extends SpeechletRequestStreamHandler {
+    private static final Set<String> supportedApplicationIds;
+
+    static {
+        /*
+         * This Id can be found on https://developer.amazon.com/edw/home.html#/ "Edit" the relevant
+         * Alexa Skill and put the relevant Application Ids in this Set.
+         */
+        supportedApplicationIds = new HashSet<>();
+        supportedApplicationIds.add("<Your application id>");
+    }
+
+    public DemoSpeechletRequestStreamHandler() {
+        super(new DemoSpeechlet(), supportedApplicationIds);
+    }
+}
